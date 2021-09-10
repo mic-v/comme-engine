e6460793e8294ffeaacfe534e39681891f937189
Michael Victorino <michaelvictorino@gmail.com>
2021-09-09 20:18:15 -0400

    Created Comme Engine folder

---
+++ comme_engine/comme_engine.cpp
@@ -0,0 +1,12 @@
+﻿// comme_engine.cpp : Defines the entry point for the application.
+//
+
+#include "comme_engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
